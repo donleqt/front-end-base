18013c7aa93a93c5dd0419fef3558888b768c261
DESKTOP-Q6EAHGS\Don Le <donleqt@gmail.com>
2017-05-18 11:46:11 +0700

    improve gulp, hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp');
-var plugins  = require("gulp-load-plugins")({
+const gulp = require('gulp');
+const child_process = require('child_process');
+const plugins  = require("gulp-load-plugins")({
     pattern: ['gulp-*', 'gulp.*']
 });
-var root = '.';
+const root = '.';
+const port = 3000;
 
 gulp.task('sass', function() {
     var localDest = root +'/css';
@@ -11,7 +13,8 @@ gulp.task('sass', function() {
         .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.rename({basename : 'style', suffix: ''}))
         .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest(localDest));
+        .pipe(gulp.dest(localDest))
+        .pipe(plugins.livereload());
 });
 
 gulp.task('templates', function() {
@@ -22,10 +25,21 @@ gulp.task('templates', function() {
         .on('error', plugins.notify.onError(function (error) {
             return 'An error occurred while compiling pug.\nLook in the console for details.\n' + error;
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.'))
+        .pipe(plugins.livereload());
 });
 
-gulp.task('default',['sass','templates'],function () {
+gulp.task('host',function () {
+    child_process.exec('ws --port ' +port, {
+        cwd: __dirname
+    })
+        .on('error',function (err) {
+            console.log(err);
+        });
+    console.log('Host listen on port ' +port);
+});
+gulp.task('default',['sass','templates','host'],function () {
+    plugins.livereload.listen();
     gulp.watch(root+'/css/**/*.scss',['sass']);
     gulp.watch([
         root+'/*.pug',
